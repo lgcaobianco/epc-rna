532dc86d76d0107f7408c4048672891bd8412b82
lgcaobianco <gu.caobianco@gmail.com>
2018-03-25 16:38:45 -0300

    O software está pronto. O módulo de geração da tabela em LaTeX também está pronto

--- epc1/src/classes/Perceptron.java
+++ epc1/src/classes/Perceptron.java
@@ -18,8 +18,8 @@ public class Perceptron {
 
 
     public void imprimirmatrizPesosInicial() {
-        for (int i = 0; i < matrizPesosInicial.length; i++) {
-            for (int j = 0; j < matrizPesosInicial[i].length; j++) {
+        for (int i = 0; i < this.matrizPesosInicial.length; i++) {
+            for (int j = 0; j < this.matrizPesosInicial[i].length; j++) {
                 System.out.print(this.matrizPesosInicial[i][j] + " ");
             }
             System.out.println();
@@ -27,8 +27,8 @@ public class Perceptron {
     }
 
     public void imprimirMatrizPontos() {
-        for (int i = 0; i < matrizPontos.size(); i++) {
-            for (int j = 0; j < matrizPontos.get(i).length; j++) {
+        for (int i = 0; i < this.matrizPontos.size(); i++) {
+            for (int j = 0; j < this.matrizPontos.get(i).length; j++) {
                 System.out.print(this.matrizPontos.get(i)[j] + " ");
             }
             System.out.println();
@@ -47,11 +47,11 @@ public class Perceptron {
 
     public void construirmatrizPesosInicial() {
         matrizPesosInicial[0][0] = -1;
-        for (int i = 1; i < matrizPesosInicial.length; i++) {
-            for (int j = 0; j < matrizPesosInicial[i].length; j++) {
+        for (int i = 1; i < this.matrizPesosInicial.length; i++) {
+            for (int j = 0; j < this.matrizPesosInicial[i].length; j++) {
                 Random r = new Random();
-                matrizPesosInicial[i][j] = r.nextDouble();
-                matrizPesosFinal[i][j] = matrizPesosInicial[i][j];
+                this.matrizPesosInicial[i][j] = r.nextDouble();
+                this.matrizPesosFinal[i][j] = this.matrizPesosInicial[i][j];
             }
         }
 
@@ -68,37 +68,28 @@ public class Perceptron {
     }
 
 
-    public int treinarPerceptron() {
+    public void treinarPerceptron() {
         double somatorio, taxaAprendizagem = 0.01, classificacao;
         int erro, iMaximo = 0;
 
         percorreLinhasConjuntoTreinamento:
-        for (int i = 0; i < matrizPontos.size(); ) {
+        for (int i = 0; i < this.matrizPontos.size(); ) {
             somatorio = 0;
-            for (int j = 0; j < matrizPontos.get(i).length - 1; j++) {
-                somatorio += (matrizPontos.get(i)[j] * matrizPesosFinal[j][0]);
+            for (int j = 0; j < this.matrizPontos.get(i).length - 1; j++) {
+                somatorio += (this.matrizPontos.get(i)[j] * this.matrizPesosFinal[j][0]);
             }
 
             classificacao = ativacao(somatorio);
 
-            if (classificacao == matrizPontos.get(i)[3]) {
+            if (classificacao == this.matrizPontos.get(i)[3]) {
                 i++;
 
             } else { //se classificacao nao coincide com d_i, ajustar coeficientes
-                erro = (int) (matrizPontos.get(i)[3] - classificacao);
-                for (int j = 0; j < matrizPesosInicial.length; j++) {
-                    matrizPesosFinal[j][0] += (taxaAprendizagem * erro * matrizPontos.get(i)[j]);
+                erro = (int) (this.matrizPontos.get(i)[3] - classificacao);
+                for (int j = 0; j < this.matrizPesosInicial.length; j++) {
+                    this.matrizPesosFinal[j][0] += (taxaAprendizagem * erro * this.matrizPontos.get(i)[j]);
                 }
                 this.contadorEpocas++;
-                if (contadorEpocas % 100000000 == 0) {
-                    System.out.println("Vetor de pesos inicial: " + matrizPesosInicial[0][0] + matrizPesosInicial[1][0] + matrizPesosInicial[2][0] + matrizPesosInicial[3][0] + ", epoca: " + contadorEpocas + ", I maximo: " + iMaximo);
-                    System.out.println("Vetor de pesos final: " + matrizPesosFinal[0][0] + matrizPesosFinal[1][0] + matrizPesosFinal[2][0] + matrizPesosFinal[3][0] + ", epoca:");
-
-                }
-                if (iMaximo < i) {
-                    iMaximo = i;
-                }
-
                 i = 0;
             }
 
@@ -106,17 +97,16 @@ public class Perceptron {
         }
 
 
-        return (int) this.contadorEpocas;
     }
 
 
     public void classificarVetores() {
         double somatorio;
 
-        for (int i = 0; i < conjuntoTeste.size(); i++) {
+        for (int i = 0; i < this.conjuntoTeste.size(); i++) {
             somatorio = 0;
-            for (int j = 0; j < conjuntoTeste.get(i).length; j++) {
-                somatorio += conjuntoTeste.get(i)[j] * matrizPesosFinal[j][0];
+            for (int j = 0; j < this.conjuntoTeste.get(i).length; j++) {
+                somatorio += this.conjuntoTeste.get(i)[j] * this.matrizPesosFinal[j][0];
             }
             int classificacao = ativacao(somatorio);
             System.out.println("A linha: " + (i + 1) + " foi classificada como: " + classificacao);
@@ -124,13 +114,23 @@ public class Perceptron {
         }
     }
 
-    public void iniciarTabelaLatex() {
-            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                    new FileOutputStream("matrizSaida.tex"), "utf-8"))) {
-                writer.write("\\begin{tabular}{llllllllll}" +  System.getProperty( "line.separator" ));
+    public static void iniciarTabelaLatex() {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("matrizSaida.tex"), "utf-8"))) {
+            writer.write("\\documentclass{article}" + System.getProperty("line.separator"));
+            writer.write("\\usepackage{multirow}" + System.getProperty("line.separator"));
+            writer.write("\\usepackage[utf8]{inputenc}" + System.getProperty("line.separator"));
+            writer.write("\\usepackage[bottom]{footmisc}" + System.getProperty("line.separator"));
+            writer.write("\\usepackage{lscape}" + System.getProperty("line.separator"));
+            writer.write("\\renewcommand\\tablename{Tabela}" + System.getProperty("line.separator"));
+            writer.write("\\begin{document}" + System.getProperty("line.separator"));
+            writer.write("\\begin{landscape}" + System.getProperty("line.separator"));
+            writer.write("\\begin{table}" + System.getProperty("line.separator"));
+            writer.write("\\centering" + System.getProperty("line.separator"));
+            writer.write("\\begin{tabular}{llllllllll}" + System.getProperty("line.separator"));
             writer.write("\\hline" + System.getProperty("line.separator"));
-            writer.write("\\multicolumn{1}{|c|}{\\multirow{2}{*}{treinamento}} & \\multicolumn{4}{l|}{Vetor de Pesos Inicial} " +
-                    "& \\multicolumn{4}{l|}{Vetor de Pesos Final} & \\multicolumn{1}{l|}{\\multirow{2}{*}{Número de Épocas}} \\\\ " +
+            writer.write("\\multicolumn{1}{|c|}{\\multirow{2}{*}{Treinamento}} & \\multicolumn{4}{l|}{Vetor de Pesos Inicial} " +
+                    "& \\multicolumn{4}{l|}{Vetor de Pesos Final} & \\multicolumn{1}{l|}{\\multirow{2}{*}{Núm. de Épocas}} \\\\ " +
                     "\\cline{2-9}\n \\multicolumn{1}{|c|}{} & \\multicolumn{1}{l|}{w_0} & \\multicolumn{1}{l|}{w_1} & " +
                     "\\multicolumn{1}{l|}{w_2} & \\multicolumn{1}{l|}{w_3} & \\multicolumn{1}{l|}{w_0} & " +
                     "\\multicolumn{1}{l|}{w_1} & \\multicolumn{1}{l|}{w_2} & \\multicolumn{1}{l|}{w_3} & " +
@@ -141,22 +141,38 @@ public class Perceptron {
         }
     }
 
-    public void preencherTabelaLatex(Perceptron p){
-        try (Writer writer = new FileWriter("matrizSaida.tex", true) ){
+    public void preencherTabelaLatex(Perceptron p, int contaQuantidadeTreinamentos) {
+        try (Writer writer = new FileWriter("matrizSaida.tex", true)) {
+            writer.append("\\multicolumn{1}{|l|}{" + (contaQuantidadeTreinamentos+1) + "} &");
 
-            for(int i=0; i<4; i++) {
-                writer.append("\\multicolumn{1}{|l|}{"+ new DecimalFormat("#.##").format(p.matrizPesosInicial[i][0])+"} & ");
+            for (int i = 0; i < 4; i++) {
+                writer.append("\\multicolumn{1}{|l|}{" + new DecimalFormat("#.####").format(p.matrizPesosInicial[i][0]) + "} & ");
             }
 
-            for(int i=0; i<4; i++){
-                writer.append("\\multicolumn{1}{|l|}{"+ new DecimalFormat("#.##").format(p.matrizPesosFinal[i][0])+"} & ");
+            for (int i = 0; i < 4; i++) {
+                writer.append("\\multicolumn{1}{|l|}{" + new DecimalFormat("#.####").format(p.matrizPesosFinal[i][0]) + "} & ");
             }
 
-            writer.append("\\multicolumn{1}{l|} {"+p.contadorEpocas +"}");
-            writer.append("\\\\" + "\\hline"+ System.getProperty("line.separator"));
-        }   catch(IOException e){
+            writer.append("\\multicolumn{1}{l|} {" + p.contadorEpocas + "}");
+            writer.append("\\\\" + "\\hline" + System.getProperty("line.separator"));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public static void finalizarTabelaLatex() {
+        try (Writer writer = new FileWriter("matrizSaida.tex", true)) {
+            writer.append("\\let\\thefootnote\\relax\\footnote{Tabela em \\LaTeX gerada automaticamente pelo software em Java}" + System.getProperty("line.separator"));
+            writer.append("\\end{tabular}" + System.getProperty("line.separator"));
+            writer.append("\\caption{Tabela em \\LaTeX{} gerada automaticamente pelo software em Java}" + System.getProperty("line.separator"));
+            writer.append("\\end{table}" + System.getProperty("line.separator"));
+            writer.append("\\end{landscape}" + System.getProperty("line.separator"));
+            writer.append("\\end{document}" + System.getProperty("line.separator"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
--- epc1/src/teste/TestarPerceptron.java
+++ epc1/src/teste/TestarPerceptron.java
@@ -2,30 +2,34 @@ package teste;
 
 import classes.Perceptron;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * * created by lgcaobianco on 21/03/18 **
  */
 public class TestarPerceptron {
     public static void main(String[] args) {
-        Perceptron perceptron = new Perceptron();
+        Perceptron.iniciarTabelaLatex();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insira a quantidade de testes");
+        int quantidadeRedes = input.nextInt();
 
-        //Necessário colocar o nome do arquivo que contém as informacoes, bem como o formato
-        perceptron.construirMatrizPontos("/home/lgcaobianco/repositorios/epc/epc1/base/valores",
-                ".csv");
-        perceptron.construirmatrizPesosInicial();
-
-        int epocas = perceptron.treinarPerceptron();
-        System.out.println("A quantidade de epocas foi: " + epocas);
-        perceptron.imprimirmatrizPesosInicial();
-
-        perceptron.construirConjuntoTeste("/home/lgcaobianco/repositorios/epc/epc1/base/conjuntoTeste",
-                ".csv");
-
-        perceptron.classificarVetores();
-
-        perceptron.iniciarTabelaLatex();
-        perceptron.preencherTabelaLatex(perceptron);
-
+        List<Perceptron> listaPerceptron = new ArrayList<Perceptron>();
+        for(int i=0; i < quantidadeRedes; i++){
+            listaPerceptron.add(new Perceptron());
+            listaPerceptron.get(i)
+                    .construirMatrizPontos("/home/lgcaobianco/repositorios/epc/epc1/base/valores",
+                    ".csv");
+            listaPerceptron.get(i).construirmatrizPesosInicial();
+            listaPerceptron.get(i).treinarPerceptron();
+            listaPerceptron.get(i).construirConjuntoTeste("/home/lgcaobianco/repositorios/epc/epc1/base/conjuntoTeste",
+                    ".csv");
+            listaPerceptron.get(i).classificarVetores();
+            listaPerceptron.get(i).preencherTabelaLatex(listaPerceptron.get(i), i);
+        }
+        Perceptron.finalizarTabelaLatex();
 
     }
 }
