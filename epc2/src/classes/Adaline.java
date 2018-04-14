d46bca9d91929d888cf2d2c596b693bec5152ee4
lgcaobianco <gu.caobianco@gmail.com>
2018-04-14 19:58:06 -0300

    EPC completo

--- epc2/src/classes/Adaline.java
+++ epc2/src/classes/Adaline.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 public class Adaline {
 	List<Double[]> inputs;
+	List<Double[]> conjuntoOperacao;
 	List<Double> coeficientesIniciais = new ArrayList<Double>();
 	List<Double> coeficientesFinais = new ArrayList<Double>();
 	List<Double> u = new ArrayList<Double>(35);
@@ -28,6 +29,11 @@ public class Adaline {
 			coeficientesFinais.add(randomDouble);
 			taxaAprendizagem = 0.0025;
 		}
+	}
+
+	public void construirConjuntoOperacao(String nomeArquivo, String tipoArquivo) {
+		LeitorPontosEntrada leitor = new LeitorPontosEntrada(nomeArquivo, tipoArquivo);
+		this.conjuntoOperacao = leitor.extrairPontos();
 	}
 
 	public void imprimirMatrizInputs() {
@@ -84,15 +90,36 @@ public class Adaline {
 		return eqm / inputs.size();
 	}
 
+	public double combinadorFinal(int linhaMatriz) {
+		double somatorio = 0;
+		for (int i = 0; i < coeficientesFinais.size(); i++) {
+			somatorio += coeficientesFinais.get(i) * conjuntoOperacao.get(linhaMatriz)[i];
+		}
+		return somatorio;
+	}
+
+	public int classificarAmostra(double u) {
+		if (u >= 0) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
 	public static void main(String[] args) {
 		double epsilon = Math.pow(10, -6);
 		List<Adaline> adalines = new ArrayList<Adaline>(5);
 		for (int i = 0; i < 5; i++) {
 			Adaline aux = new Adaline("/home/lgcaobianco/repositorios/epc-rna/epc2/src/classes/base/inputs", ".csv");
+			aux.construirConjuntoOperacao("/home/lgcaobianco/repositorios/epc-rna/epc2/src/classes/base/operacao",
+					".csv");
 			adalines.add(aux);
 		}
 
 		for (Adaline adaline : adalines) {
+			System.out.println("====================== Adaline ======================");
+			// fase de treino
+			System.out.println("               Treinamento               ");
 			adaline.erroQuadraticoAtual = adaline.calcularErroQuadratico();
 			while (Math.abs(adaline.erroQuadraticoAtual - adaline.erroQuadraticoAnterior) > epsilon) {
 				adaline.treinarAdaline();
@@ -100,11 +127,27 @@ public class Adaline {
 				adaline.erroQuadraticoAtual = adaline.calcularErroQuadratico();
 				adaline.contadorEpocas++;
 			}
-			System.out.println("Os coeficientes finais são: ");
+			System.out.print("Os coeficientes inciais são: ");
+			adaline.imprimirMatrizCoeficientes("inicial");
+
+			System.out.print("Os coeficientes finais são: ");
 			adaline.imprimirMatrizCoeficientes("final");
-			System.out.println("O erro quadratico terminou em: " + adaline.erroQuadraticoAtual + "com: "
-					+ adaline.contadorEpocas + " epocas.");
+			System.out.println(
+					"O erro quadratico terminou em: " + (adaline.erroQuadraticoAtual - adaline.erroQuadraticoAnterior)
+							+ " com: " + adaline.contadorEpocas + " epocas.");
+			System.out.println("                                         ");
 			System.out.println();
+
+			// fase de operacao
+			System.out.println("                Operação                ");
+
+			for (int i = 0; i < adaline.conjuntoOperacao.size(); i++) {
+				int resultado = adaline.classificarAmostra(adaline.combinadorFinal(i));
+				System.out.println((i + 1) + ", " + resultado);
+			}
+			System.out.println("                                        ");
+			System.out.println("=====================================================");
+
 		}
 	}
 
